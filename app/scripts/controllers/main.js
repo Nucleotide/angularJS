e003e6d9b2f34322d157ff4f1249c847d447f78d
Jone <joonas.laakkonen@gmail.com>
2014-05-07 13:02:09 +0300

    Ready to start fixing backend

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,21 @@
 var app = angular.module('frontendApp');
 
 app.factory('Blogs', function($http){
-    var URL = 'https://ng-project-backend.herokuapp.com/api/blogs.json';
+    var URL_BASE = 'https://ng-project-backend.herokuapp.com/api/blogs';
 
     var blogsService = {};
 
     blogsService.all = function(){
-        return $http.get(URL);
+        return $http.get(URL_BASE+'.json');
       };
 
     blogsService.create = function(data){
         console.log('called');
-        return $http.post(URL, data);
+        return $http.post(URL_BASE+'.json', data);
+      };
+
+    blogsService.delete = function(data){
+        return $http.delete(URL_BASE+'/'+data.id+'.json', data);
       };
 
     return blogsService;
@@ -21,12 +25,62 @@ app.factory('Blogs', function($http){
   });
 
 
-app.controller('MainCtrl', function ($scope, Blogs) {
-    
+app.factory('Auth', function($http){
+    var URL = 'http://ng-project-backend.herokuapp.com/session';
+    var service = {};
+
+    service.logged = {};
+
+    service.login = function(credentials) {
+      return $http.post(URL, credentials).then(
+        function (token) {
+          service.logged.status = true;
+          service.logged.user = credentials.user;
+          $http.defaults.headers.common['auth-token'] = token.data;
+          return token.data;
+        }
+      );
+    };
+
+    service.logout = function(credentials) {
+      return $http.delete(URL).then(
+        function () {
+          service.logged.status = false;
+          service.logged.user = null;
+          delete $http.defaults.headers.common['auth-token'];
+          return null;
+        }
+      );
+    };
+
+    return service;
+  });
+
+
+app.controller('MainCtrl', function ($scope, Blogs, Auth) {
+    $scope.loggedIn = Auth.logged;
+
+    $scope.logout = function() {
+        Auth.logout();
+      };
+
+    $scope.login = function(){
+        Auth.login($scope.credentials)
+        .then(
+        function(data) {
+          console.log(data);
+        },function( data ) {
+          // failure
+        }
+      );
+        $scope.credentials = {};
+      };
+
     $scope.formVisible = false;
 
     Blogs.all().success( function(data, status, headers, config) {
         $scope.entries = data;
+        console.log(data);
       });
 
     $scope.createBlog = function() {
@@ -38,4 +92,12 @@ app.controller('MainCtrl', function ($scope, Blogs) {
         $scope.formVisible = false;
         $scope.blog = {};
       };
+
+    $scope.deleteBlog = function(entry) {
+        Blogs.delete(entry).success(function(){
+          var index = $scope.entries.indexOf(entry);
+          $scope.entries.splice(index, 1);
+        });
+      };
+
   });
